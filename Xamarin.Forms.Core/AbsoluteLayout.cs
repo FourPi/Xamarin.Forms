fd2dc56601fa106a62df8ba9b2087a8a1b3e5fb7
Matthew Percy <matt@4pi.com.au>
2017-05-22 14:52:17 +1000

    Removed rounding in AbsoluteLayout to ensure Android devices with certain sizes can still use proportional sizes to fill the device screen

--- Xamarin.Forms.Core/AbsoluteLayout.cs
+++ Xamarin.Forms.Core/AbsoluteLayout.cs
@@ -221,7 +221,7 @@ namespace Xamarin.Forms
 
 			if (widthIsProportional)
 			{
-				result.Width = Math.Round(region.Width * bounds.Width);
+				result.Width = region.Width * bounds.Width;
 			}
 			else if (bounds.Width != AutoSize)
 			{
@@ -230,7 +230,7 @@ namespace Xamarin.Forms
 
 			if (heightIsProportional)
 			{
-				result.Height = Math.Round(region.Height * bounds.Height);
+				result.Height = region.Height * bounds.Height;
 			}
 			else if (bounds.Height != AutoSize)
 			{
@@ -262,7 +262,7 @@ namespace Xamarin.Forms
 
 			if (xIsProportional)
 			{
-				result.X = Math.Round((region.Width - result.Width) * bounds.X);
+				result.X = (region.Width - result.Width) * bounds.X;
 			}
 			else
 			{
@@ -271,7 +271,7 @@ namespace Xamarin.Forms
 
 			if (yIsProportional)
 			{
-				result.Y = Math.Round((region.Height - result.Height) * bounds.Y);
+				result.Y = (region.Height - result.Height) * bounds.Y;
 			}
 			else
 			{
